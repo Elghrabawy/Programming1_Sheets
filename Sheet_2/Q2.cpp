b42dd9dd36c10052a759777672f8790f89ca761d
Ebrahim Elghrbawy <HIMA132004@GMAIL.COM>
2023-11-23 20:57:17 +0200

    Solve Q2

--- Sheet_2/Q2.cpp
+++ Sheet_2/Q2.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int coinToss(){
+    return 1 + rand() % 2;
+}
 int main()
 {
+    int timesOfTossing, curToss;
+    cout << "How many times the coin should be tossed ? ";
+    cin >> timesOfTossing;
 
+
+    for(int i = 1; i <= timesOfTossing; i++){
+        curToss = coinToss();
+
+        cout << i << " : ";
+
+        switch (curToss)
+        {
+        case 1:
+            cout << "Heads\n";
+            break;
+        case 2:
+            cout << "Tails\n";
+            break;
+        }
+    }
 }
